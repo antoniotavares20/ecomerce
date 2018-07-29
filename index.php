37d965fdcef4332dbaa215db05d0349414222673
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-07-29 16:13:44 -0300

    reconfigurou funcionamento da pagina com slin

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php 
-
 require_once("vendor/autoload.php");
-//emprego somente dessa linha
 use \Slim\Slim;
-//emprego somente dessa classe
 use \Hcode\Page;
-//carrega as paginas desejadas
-$app = new \Slim\Slim();
-//recebe a configuração
+$app = new Slim();
 $app->config('debug', true);
-//determina a raiz das rotas
 $app->get('/', function() {
-//carregas a paginas
-	//cria o construtor da pagina
-    $page =  new Page();
-//carrega a pagina desejada
-    $page->setTpl("index");
-});
-
+    
+	$page = new Page();
+	$page->setTpl("index");
+}); 
 $app->run();
-
  ?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,49 +1,37 @@
-<?php
-
+<?php 
 namespace Hcode;
-	
 use Rain\Tpl;
-
-class Page{
+class Page {
 	private $tpl;
-	private $defauts = [];
-	private $options = [
-		"data" =>[]
+	private $options = [];
+	private $defaults = [
+		"data"=>[]
 	];
-
 	public function __construct($opts = array()){
-		//faz o merge entre duas variaveis e guarda em options
-		$this->options = array_marge($this->defauts, $opts);
-
-		$coding = array(
-						"tpl_dir"   =>$_SERVER["DOCUMENT_ROOT"]."/views/",
-						"cache_dir" =>$_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-						"debug"		=> false
-	);
-
-		Tpl::configure($coding);
-		
-		$this->tpl = new tpl;	
+		$this->options = array_merge($this->defaults, $opts);
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+	    );
+		Tpl::configure( $config );
+		$this->tpl = new Tpl;
 		$this->setData($this->options["data"]);
-
-		$this->draw("header");
-
+		$this->tpl->draw("header");
 	}
-	private function setData($data = array()){
+	private function setData($data = array())
+	{
 		foreach ($data as $key => $value) {
-			$this->tpl-assign($key, $value);
+			$this->tpl->assign($key, $value);
 		}
 	}
-
-
-	public function setTpl($name, $data = array(), $returnHTLM = false){
+	public function setTpl($name, $data = array(), $returnHTML = false)
+	{
 		$this->setData($data);
-		$this->tpl->draw($name, $returnHTLM);
-	return $this->tpl->draw($name, $returnHTLM);
+		return $this->tpl->draw($name, $returnHTML);
 	}
-
 	public function __destruct(){
 		$this->tpl->draw("footer");
 	}
 }
-?>
+ ?>
