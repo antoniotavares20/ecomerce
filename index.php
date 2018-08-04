648a52a69403c290edbd83110245b9f0365c1fb2
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-08-04 17:44:07 -0300

    Adicionou lista de produtos na home

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use \Hcode\Page;
 $app = new Slim();
 $app->config('debug', true);
 
-
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
